218b62fbfb5ab3ffd270e1fe7efe8b23c837ff44
jdamilasp <jdamilasp@gmail.com>
2019-02-16 19:31:24 +0530

    api-doc added

---
+++ server/public/apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "PRODUCT API DOC",
+  "version": "1.0.0",
+  "description": "Product API Documentation",
+  "apidoc": "0.3.0",
+  "sampleUrl": "http://localhost:3000",
+  "defaultVersion": "0.0.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-02-16T14:00:54.918Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
